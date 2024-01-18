da2b0684618a47c8250750d50bf068db9f366c6b
frsargua <frsargua@gmail.com>
2024-01-18 19:57:56 +0000

    hello world console log 3

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  connsole.log('hello world');
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
+console.log('hello world');
