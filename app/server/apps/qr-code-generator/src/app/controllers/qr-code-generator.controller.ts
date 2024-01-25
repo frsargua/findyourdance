ca494a76b8e9cbc00584d54ec4f73784b7d80599
frsargua <frsargua@gmail.com>
2024-01-25 22:22:40 +0000

    create barcode creator service

--- app/server/apps/qr-code-generator/src/app/controllers/qr-code-generator.controller.ts
+++ app/server/apps/qr-code-generator/src/app/controllers/qr-code-generator.controller.ts
@@ -2,16 +2,26 @@ import { Body, Controller, Post, Res } from '@nestjs/common';
 import { QrCodeGeneratorService } from '../services/qr-code-generator.service';
 import { Response } from 'express';
 
-@Controller('generate/code/qr')
+@Controller('generate/code/')
 export class QrCodeGeneratorController {
   constructor(
     private readonly qrCodeGeneratorService: QrCodeGeneratorService
   ) {}
 
-  @Post()
+  @Post('qrcode')
   async generateQrCode(@Body('uuid') uuid: string, @Res() response: Response) {
     const qrCodeImage = await this.qrCodeGeneratorService.createQRcode(uuid);
-    response.setHeader('Content-Type', 'image/png');
+    response.setHeader('Content-Type', 'image/svg');
+    console.log(qrCodeImage);
     response.send(qrCodeImage);
+  }
+
+  @Post('barcode')
+  async generateBarcode(@Body('uuid') uuid: string, @Res() response: Response) {
+    const barcodeImage = await this.qrCodeGeneratorService.createBarcode(uuid);
+    response.setHeader('Content-Type', 'image/svg');
+    console.log(barcodeImage);
+
+    response.send(barcodeImage);
   }
 }
