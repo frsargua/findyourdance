a31ab9945b56493e0a68fc819ec945d8ae92a6a6
frsargua <frsargua@gmail.com>
2024-09-17 21:24:37 +0100

    deserialises colum names

--- app/server/apps/events/src/main.ts
+++ app/server/apps/events/src/main.ts
@@ -1,13 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { EventsModule } from './app/modules/events.module';
 import { Logger } from 'nestjs-pino';
 import { HTTP_PORT } from './app/constants/config.constants';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(EventsModule);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   const configService = app.get(ConfigService);
   app.use(cookieParser());
   app.useGlobalPipes(
