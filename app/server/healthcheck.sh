120d8b5c044e3cdae900c7487511d8c3e2341c1b
frsargua <frsargua@gmail.com>
2024-09-30 00:09:38 +0100

    saved database name as a variable

--- app/server/healthcheck.sh
+++ app/server/healthcheck.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+DB_NAME="findyourdance"
+
 # Check if PostgreSQL is ready
 pg_isready -U postgres || exit 1
 
 # Check if the database exists and PostGIS is installed
-psql -U postgres -d findyourdance -c "SELECT postgis_version();" > /dev/null 2>&1
+psql -U postgres -d "$DB_NAME" -c "SELECT postgis_version();" > /dev/null 2>&1
 
 # Store the exit status
 exit_status=$?
 
 if [ $exit_status -eq 0 ]; then
-    echo "Database 'findyourdance' exists and PostGIS is installed."
+    echo "Database '$DB_NAME' exists and PostGIS is installed."
     exit 0
 else
-    echo "Database 'findyourdance' does not exist or PostGIS is not installed."
+    echo "Database '$DB_NAME' does not exist or PostGIS is not installed."
     exit 1
 fi
