96563eaedf2637110a1674db8ccb54d7769afe36
frsargua <frsargua@gmail.com>
2024-10-10 00:51:36 +0100

    updated abstract repository interface

--- app/server/libs/common/src/database/interfaces/base.interfaces.ts
+++ app/server/libs/common/src/database/interfaces/base.interfaces.ts
@@ -5,13 +5,13 @@ export interface BaseInterfaceRepository<T> {
   createMany(data: DeepPartial<T>[]): T[];
   save(data: DeepPartial<T>): Promise<T>;
   saveMany(data: DeepPartial<T>[]): Promise<T[]>;
-  findOneById(id: string): Promise<T>;
-  findByCondition(filterCondition: FindOneOptions<T>): Promise<T>;
+  findOneById(id: string): Promise<T | null>;
+  findByCondition(filterCondition: FindOneOptions<T>): Promise<T | null>;
   findAll(options?: FindManyOptions<T>): Promise<T[]>;
   remove(data: T): Promise<T>;
   findWithRelations(relations: FindManyOptions<T>): Promise<T[]>;
-  preload(entityLike: DeepPartial<T>): Promise<T>;
-  findOne(options: FindOneOptions<T>): Promise<T>;
+  preload(entityLike: DeepPartial<T>): Promise<T | null>;
+  findOne(options: FindOneOptions<T>): Promise<T | null>;
   queryWithQueryBuilder(
     queryCallback: (queryBuilder: any) => any
   ): Promise<T[]>;
