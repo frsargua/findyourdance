118a3ea0be977986afa1f89578c61fe07f056c49
frsargua <frsargua@gmail.com>
2024-10-10 00:54:34 +0100

    updated module to import loggerModule with pino-logger

--- app/server/apps/auth/src/app/modules/users.module.ts
+++ app/server/apps/auth/src/app/modules/users.module.ts
@@ -3,12 +3,17 @@ import { UsersController } from '../controllers/users.controller';
 import { UsersService } from '../services/users.service';
 import { UsersRepository } from '../repository/users.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DatabaseModule, User } from '@app/common';
+import { DatabaseModule, LoggerModule, User } from '@app/common';
 import { AddressModule } from './address-user.module';
 import { AddressUserService } from '../services/address-user.service';
 
 @Module({
-  imports: [DatabaseModule, AddressModule, TypeOrmModule.forFeature([User])],
+  imports: [
+    DatabaseModule,
+    LoggerModule,
+    AddressModule,
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository, AddressUserService],
   exports: [UsersService],
